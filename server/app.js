b9a0a9b9997fd8d7853ee1240b79aa051bb9affc
TRomesh <tharakaromesh@gmail.com>
2017-05-20 00:43:45 +0530

    add middlewares for sever

--- server/app.js
+++ server/app.js
@@ -2,9 +2,16 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const port = 3030;
 let onlineUsers = [];
 let sockets = {};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended:true
+}));
 
 server.listen(port,()=>{console.log('App running on port :'+port)});
 
