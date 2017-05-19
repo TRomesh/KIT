105626200c0a5ca6a0d116d37f56eaadb8c637e9
TRomesh <tharakaromesh@gmail.com>
2017-05-19 23:08:03 +0530

    add dependencies

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Keep In Touch</h2>
+        </div>
+        <p >
+          Keep In Touch Chat Application
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
