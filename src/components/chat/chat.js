174ffb3c7c293cbc0d3eee7b64e72cb443be7595
TRomesh <tharakaromesh@gmail.com>
2017-05-20 16:09:33 +0530

    add material-ui

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 100,
+  width: 100,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+
 
 class Chat extends Component {
   render() {
     return (
-      <div>
-        Chat..
-      </div>
+    <div>
+     <Paper style={style} zDepth={1} />
+    </div>
     );
   }
 }
